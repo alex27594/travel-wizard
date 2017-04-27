e5eb5dec5fdc013841898f1ac438f3760f446188
Alexander Sukhochev <alex27594@gmail.com>
2017-04-27 14:31:44 +0300

    last wins

--- source/estimators.py
+++ source/estimators.py
@@ -2,37 +2,67 @@ import math
 import numpy as np
 import time
 import pandas
+import shutil
+import os
+import filters
+import whoosh
 
+from functools import reduce
 from sklearn.neighbors import LSHForest
 from sklearn.base import BaseEstimator
 from sklearn.base import ClassifierMixin
 from sklearn.naive_bayes import MultinomialNB
 from collections import Counter
 from collections import OrderedDict
+from whoosh.index import create_in
+from whoosh.index import open_dir
+from whoosh.fields import *
+from whoosh.analysis import SpaceSeparatedTokenizer
+from whoosh.analysis import LowercaseFilter
+from whoosh.writing import BufferedWriter
+from whoosh.index import LockError
+from whoosh.qparser import QueryParser
+from whoosh import qparser
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from whoosh.scoring import TF_IDF
 
-
-
-def gaussian_parzen_function(r):
-    return math.pi**(-0.5) * math.exp(-0.5*r*r)
 
 def epan_parzen_function(r):
     return 3/4*(1 - r*r)
 
 
+def quart_parzen_function(r):
+    return 15/16*(1 - r*r)*(1 - r*r)
+
+
+def trian_parzen_function(r):
+    return 1 - abs(r)
+
+
+def gauss_parzen_function(r):
+    return (2 * math.pi)**(-0.5)*math.exp(-0.5*r*r)
+
+
 class LSHNearestNeighbors(BaseEstimator, ClassifierMixin):
 
-    def __init__(self, n_estimators=10, n_candidates=50, n_neighbors=9, mode="most_common", answers="one"):
+    def __init__(self, n_estimators=10, n_candidates=50, n_neighbors=9, mode="most_common", parzen_func=epan_parzen_function, answers="one"):
         self.n_estimator = n_estimators
         self.n_candidates = n_candidates
         self.n_neighbors = n_neighbors
-        assert mode in ["most_comon", "parzen_window"]
+        self.parzen_func = parzen_func
+        assert mode in ["most_common", "parzen_window", "dann", "voting_parzen_window"]
         self.mode = mode
         assert answers in ["one", "many"]
         self.answers = answers
 
     def fit(self, X, y):
         self.INNER_NEIGHBORS = 50
-        self.lshforest_ = LSHForest(n_estimators=self.n_estimator, n_candidates=self.n_candidates,
+        if self.mode == "dann":
+            self.lshforest_ = LSHForest(n_estimators=self.n_estimator, n_candidates=self.n_candidates,
+                                    n_neighbors=self.INNER_NEIGHBORS)
+        else:
+            self.lshforest_ = LSHForest(n_estimators=self.n_estimator, n_candidates=self.n_candidates,
                                     n_neighbors=self.n_neighbors)
         self.lshforest_.fit(X, y)
         self.X_ = X
@@ -73,9 +103,39 @@ class LSHNearestNeighbors(BaseEstimator, ClassifierMixin):
                 if self.answers == "one":
                     for i in range(self.n_neighbors):
                         if k_n_d[i][0] not in rating.keys():
-                            rating[k_n_d[i][0]] = epan_parzen_function(k_n_d[i][1]/k_n_d[self.n_neighbors - 1][1])
+                            rating[k_n_d[i][0]] = self.parzen_func(k_n_d[i][1]/(k_n_d[self.n_neighbors - 1][1] + 0.0001))
                         else:
-                            rating[k_n_d[i][0]] += epan_parzen_function(k_n_d[i][1]/k_n_d[self.n_neighbors - 1][1])
+                            rating[k_n_d[i][0]] += self.parzen_func(k_n_d[i][1]/(k_n_d[self.n_neighbors - 1][1] + 0.0001))
+                        if rating[k_n_d[i][0]] > max_rate:
+                            max_rate = rating[k_n_d[i][0]]
+                            max_ind = k_n_d[i][0]
+                    self.times.append(time.time() - t1)
+                    y_pred.append(max_ind)
+                elif self.answers == "many":
+                    for i in range(self.n_neighbors):
+                        if k_n_d[i][0] not in rating.keys():
+                            rating[k_n_d[i][0]] = self.parzen_func(k_n_d[i][1]/k_n_d[self.n_neighbors - 1][1])
+                        else:
+                            rating[k_n_d[i][0]] += self.parzen_func(k_n_d[i][1]/k_n_d[self.n_neighbors - 1][1])
+                    sum_weights = sum(rating[k] for k in rating)
+                    for k in rating:
+                        rating[k] /= sum_weights
+                    self.times.append(time.time() - t1)
+                    y_pred.append(rating)
+        elif self.mode == "voting_parzen_window":
+            for x in X:
+                t1 = time.time()
+                k_n_d = self.get_neighbors_n_dists(x)
+                rating = OrderedDict()
+                parzen_functions = [epan_parzen_function, quart_parzen_function, trian_parzen_function, gauss_parzen_function]
+                max_ind = -1
+                max_rate = -1
+                if self.answers == "one":
+                    for i in range(self.n_neighbors):
+                        if k_n_d[i][0] not in rating.keys():
+                            rating[k_n_d[i][0]] = sum(map(lambda func: func(k_n_d[i][1]/k_n_d[self.n_neighbors - 1][1]), parzen_functions))
+                        else:
+                            rating[k_n_d[i][0]] += sum(map(lambda func: func(k_n_d[i][1]/k_n_d[self.n_neighbors - 1][1]), parzen_functions))
                         if rating[k_n_d[i][0]] > max_rate:
                             max_rate = rating[k_n_d[i][0]]
                             max_ind = k_n_d[i][0]
@@ -92,7 +152,24 @@ class LSHNearestNeighbors(BaseEstimator, ClassifierMixin):
                         rating[k] /= sum_weights
                     self.times.append(time.time() - t1)
                     y_pred.append(rating)
+
+        """
+        elif self.mode == "dann":
+            for x in X:
+                k_n_d_n_c = self.get_neighbors_n_dists_n_coordinates(x)
+                sigma = np.eye(self.INNER_NEIGHBORS)
+                sqrt_sigma = np.linalg.sqrtm(sigma)
+                d = [np.linalg.norm(np.dot(sqrt_sigma,x - k_n_d_n_c[i][2])) for i in range(len(k_n_d_n_c))]
+                h = max(d)
+                w = [(1 - (d[i]/h)**3)**3 for i in range(self.INNER_NEIGHBORS)]
+                pi = OrderedDict()
+                for key in [k_n_d_n_c[i][0] for i in range(len())]:
+                    pi[key] = sum(w[i] for i in range(len(w)) if k_n_d_n_c[i][0] == key)/sum(w)
+                B = sum(pi[i] * np.dot((k_n_d_n_c[i][2] - x).reshape(len(k_n_d_n_c[i][2], 1)),
+                                       (k_n_d_n_c[i][2] - x).reshape(1, len(k_n_d_n_c[i][2))) for i in range(len(k_n_d_n_c)))
+        """
         return y_pred
+
 
 
 class Bayes_Wrapper(BaseEstimator, ClassifierMixin):
@@ -160,11 +237,105 @@ class MixedClassifier:
         return y_pred
 
 
+class WhooshClassifier(BaseEstimator, ClassifierMixin):
+    def fit(self, df):
+        schema = Schema(content=TEXT(stored=True, analyzer=SpaceSeparatedTokenizer() | filters.MystemFilter() | filters.TranslateFilter()), id=ID(stored=True))
+        if os.path.exists("indexdir"):
+            shutil.rmtree("indexdir")
+        os.mkdir("indexdir")
+        self.ix = create_in("indexdir", schema)
+        writer = self.ix.writer()
+        for d in df.iterrows():
+            writer.add_document(content=d[1]["query"], id=str(d[1]["yaHotelId"]))
+        writer.commit()
+        return self
+
+    def predict(self, df):
+        parser = QueryParser("content", schema=self.ix.schema, group=qparser.OrGroup)
+        y = []
+        with self.ix.searcher(weighting=whoosh.scoring.Frequency) as searcher:
+            for q in df.iterrows():
+                print("start")
+                t1 = time.time()
+                query = parser.parse(q[1]["query"])
+                results = [(int(res["id"]), res.score) for res in searcher.search(query, limit=10)]
+                print("pretty finish", time.time() - t1)
+                rating = OrderedDict()
+                max_rate = -1
+                max_ind = -1
+                for i in range(len(results)):
+                    if results[i][0] in rating:
+                        rating[results[i][0]] += epan_parzen_function(results[i][1])
+                    else:
+                        rating[results[i][0]] = epan_parzen_function(results[i][1])
+                    if rating[results[i][0]] > max_rate:
+                        max_rate = rating[results[i][0]]
+                        max_ind = results[i][0]
+                print("finish", time.time() - t1)
+                y.append(max_ind)
+        return y
 
 
 if __name__ == "__main__":
-    import sklearn
-    print(sklearn.__version__)
+    import preprocessors
 
+    from sklearn.feature_extraction.text import TfidfVectorizer
+    from sklearn.model_selection import train_test_split
+    from sklearn.metrics import accuracy_score
+    from sklearn.metrics import precision_score
+    from sklearn.metrics import recall_score
+    from sklearn.model_selection import GridSearchCV
 
+    path = "query.yaHotelId.showInTop.sure.final.tsv"
+    df = pandas.read_csv(path, sep="\t")
+    print("Изначальная размерность данных:", df.shape, ";", "Количество отелей:", len(df["yaHotelId"].unique()))
 
+    sure_df = df[df["sure"]]
+    filtered_values = [value[0] for value in sure_df["yaHotelId"].value_counts().iteritems() if value[1] >= 5]
+    filtered_df = sure_df[sure_df["yaHotelId"].isin(filtered_values)]
+    print("Получившаяся размерность данных:", filtered_df.shape, ";", "Количество отелей:", len(filtered_df["yaHotelId"].unique()))
+
+    df_train, df_test = train_test_split(filtered_df, test_size=0.01)
+    df_train.reset_index(drop=True, inplace=True)
+    df_test.reset_index(drop=True, inplace=True)
+
+    ids = set(df_train["yaHotelId"])
+    df_hotels = pandas.read_csv("hotel_all.csv")
+    df_hotels = df_hotels[df_hotels["id"].isin(ids)]
+    df_hotels.rename(columns={"id": "yaHotelId", "all": "query"}, inplace=True)
+    df_hotels.drop(labels=["Unnamed: 0"], axis=1, inplace=True)
+    df_hotels.dropna(axis=0, how="any", inplace=True)
+
+    df_tr = df_train.append(df_hotels)
+    df_tr.reset_index(drop=True, inplace=True)
+
+    prep = preprocessors.Preprocessor([preprocessors.num_filter_preprocess, preprocessors.mystem_preprocess, preprocessors.dictionary_preprocess])
+    vectorizer = TfidfVectorizer(preprocessor=prep.preprocess)
+    y_train = np.array(df_tr["yaHotelId"])
+    X_train = vectorizer.fit_transform(df_tr["query"])
+
+    y_test = np.array(df_test["yaHotelId"])
+    X_test = vectorizer.transform(df_test["query"])
+
+    """
+    param_grid = [{"n_estimators": [15], "n_candidates": [200], "n_neighbors": [5, 9, 15], "mode": ["parzen_window"],
+                   "parzen_func": [epan_parzen_function, quart_parzen_function, trian_parzen_function, gauss_parzen_function],
+                   },
+                  {"n_estimators": [15], "n_candidates": [200], "n_neighbors": [5, 9, 15], "mode": ["voting_parzen_window"]}]
+
+    clf = LSHNearestNeighbors()
+    cv = GridSearchCV(clf, param_grid=param_grid, verbose=5)
+    cv.fit(X_train, y_train)
+    print(cv.best_score_)
+    print(cv.best_params_)
+
+    """
+    clf = LSHNearestNeighbors(n_estimators=15, n_candidates=200, n_neighbors=9, mode="parzen_window", parzen_func=epan_parzen_function)
+    clf.fit(X_train, y_train)
+
+    y_pred = clf.predict(X_test)
+    print("accuracy:", accuracy_score(y_test, y_pred))
+
+    print("precision:", precision_score(y_test, y_pred, average="weighted"))
+
+    print("recall:", recall_score(y_test, y_pred, average="weighted"))
--- source/preprocessors.py
+++ source/preprocessors.py
@@ -2,11 +2,13 @@ import json
 import functools
 import time
 import pandas
+import numpy as np
 
 from pymystem3 import Mystem
 from functools import reduce
-
-
+from collections import Counter
+"""
+#for hotel classification
 def get_mystem_decorator(func):
     mystem = Mystem()
     @functools.wraps(func)
@@ -15,7 +17,7 @@ def get_mystem_decorator(func):
     return new_func
 
 def get_dictionary_decorator(func):
-    with open("dictionary.txt") as f:
+    with open("new_dictionary.txt") as f:
         dictionary = json.loads(f.readline())
     @functools.wraps(func)
     def new_func(string):
@@ -28,14 +30,34 @@ def text_preprocess(mystem, string):
     #сюда можно добавить и прочую обработку, например использовать API яндекс.переводчика;
     #сейчас этот метод уменьшает размерность на 1505 единиц
     return " ".join(list(map(lambda st: mystem.lemmatize(st)[0], string.split())))
+"""
+MYSTEM = Mystem()
 
-@get_mystem_decorator
-def mystem_preprocess(mystem, string):
-    return " ".join(list(map(lambda st: mystem.lemmatize(st)[0], string.split())))
+with open("../data/new_dictionary1.txt") as f:
+    DICTIONARY = json.loads(f.readline())
 
-@get_dictionary_decorator
-def dictionary_preprocess(dictionary, string):
-    return " ".join(list(map(lambda st: dictionary[st] if st in dictionary else st, string.split())))
+REGIONS = set(region.lower() for region in pandas.read_csv("../data/regions.tsv", sep="\t", names=["id", "type", "name"])["name"])
+
+
+def mystem_preprocess(string):
+    return " ".join(list(map(lambda st: MYSTEM.lemmatize(st)[0], string.split())))
+
+
+def dictionary_preprocess(string):
+    return " ".join(list(map(lambda st: DICTIONARY[st] if st in DICTIONARY else st, string.split())))
+
+
+def num_filter_preprocess(string):
+    return " ".join(list(filter(lambda st: not st.isnumeric() or len(st) < 4, string.split())))
+
+
+def buildup_set(df):
+    length = df.shape[0]
+    new_df1 = pandas.DataFrame([[df["yaHotelId"].iloc[i], " ".join(df["query"].iloc[i].strip().split()[:-1])] for i in range(length)],
+                                   columns=["yaHotelId", "query"])
+    new_df2 = pandas.DataFrame([[df["yaHotelId"].iloc[i], " ".join(df["query"].iloc[i].strip().split()[:-2])] for i in range(length)],
+                                   columns=["yaHotelId", "query"])
+    return df.append(new_df1).append(new_df2)
 
 
 class Preprocessor:
@@ -51,5 +73,103 @@ class Preprocessor:
         res = reduce(lambda x, y: y(x), [string] + self.pipeline)
         self.times.append(time.time() - t1)
         return res
+
+
+#for non-hotel classification
+
+def word_count_feature(line):
+    return len(str(line).strip().split())
+
+
+def isenglish_feature(line):
+    latin_chars = set(list("abcdefghikjlmnopqrstvuwxyz"))
+    for word in str(line).strip().split():
+        for ch in word:
+            if ch in latin_chars:
+                return 1
+    return 0
+
+
+def istitle_feature(line):
+    for word in str(line).strip().split():
+        if word.istitle():
+            return 1
+    return 0
+
+
+class IsWordFeature:
+    def __init__(self, word):
+        self.word = word
+
+    def __call__(self, line):
+        if self.word in list(map(lambda st: MYSTEM.lemmatize(st)[0], str(line).strip().split())):
+            return 1
+        return 0
+
+
+def many_feature(line):
+    for word in str(line).strip().split():
+        word_information = MYSTEM.analyze(word)[0]
+        if "analysis" in word_information and len(word_information["analysis"]) > 0 \
+                and "gr" in word_information["analysis"][0] and "мн" in word_information["analysis"][0]["gr"]:
+            return 1
+    return 0
+
+
+def bastard_words_feature(line):
+    for word in str(line).strip().split():
+        word_information = MYSTEM.analyze(word)[0]
+        if "analysis" in word_information and len(word_information["analysis"]) > 0 and\
+                        "qual" in word_information["analysis"][0] and word_information["analysis"][0]["qual"] == "bastard":
+            return 1
+    return 0
+
+
+def isdigit_feature(line):
+    for word in str(line).strip().split():
+        if word.isnumeric():
+            return 1
+    return 0
+
+
+def isregion_feature(line):
+    for word in str(line).strip().split():
+        if MYSTEM.lemmatize(word)[0] in REGIONS:
+            return 1
+    return 0
+
+
+def isverb_feature(line):
+    for word in str(line).strip().split():
+        word_information = MYSTEM.analyze(word)[0]
+        if "analysis" in word_information and len(word_information["analysis"]) > 0 \
+                and "gr" in word_information["analysis"][0] and "V," in word_information["analysis"][0]["gr"]:
+            return 1
+    return 0
+
+
+def isadjective_feature(line):
+    for word in str(line).strip().split():
+        word_information = MYSTEM.analyze(word)[0]
+        if "analysis" in word_information and len(word_information["analysis"]) > 0 \
+                and "gr" in word_information["analysis"][0] and "A=" in word_information["analysis"][0]["gr"]:
+            return 1
+    return 0
+
+
+class NonHotelPreprocessor:
+
+    def __init__(self, funcs):
+        self.funcs = funcs
+
+    def preprocess(self, lines):
+        features = []
+        for line in lines:
+            line_features = list(map(lambda func: func(line), self.funcs))
+            features.append(line_features)
+        return np.array(features)
+
+
+
 
 
